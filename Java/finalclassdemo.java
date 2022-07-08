da1aa675e24f377942002056cee4acafc442e4b8
parmar <ppb5787@gmail.com>
2022-07-08 11:15:35 +0530

    2nd file added

--- Java/finalclassdemo.java
+++ Java/finalclassdemo.java
@@ -8,6 +8,7 @@ class finalclassdemo{
     public static void main(String[] args){
         B b=new B();
         System.out.println(b.y);
+	System.out.println("Hello");
     }
 }
 //it can not be inherited
